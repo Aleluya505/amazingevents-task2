4c58b5ed9681bad41e8fce7ee92b808259c5ed51
Alebarr <barraganalejandra77@gmail.com>
2023-03-07 14:35:24 -0300

    into de functions de busqueda

--- js/index.js
+++ js/index.js
@@ -1,99 +1,92 @@
 let indexHome = "";
-let tarjets = document.getElementById("container-cards")
-    for (let event of data.events){
-        indexHome += createCard (event)  
-    }
-console.log (indexHome)
-tarjets.innerHTML += indexHome;    
-//generé un let de las tarjetas para que tome los
-// elementos del id del index.html   
+let tarjets = document.getElementById("container-cards");
 
+for (let event of data.events) {
+  indexHome += createCard(event);
+}
 
-//categorias
+console.log(indexHome);
+tarjets.innerHTML += indexHome;
+
 let categorias = [];
-data.events.forEach (event => {  
+data.events.forEach(event => {  
   if(!categorias.includes(event.category)){
     categorias.push(event.category);
   }
 }); 
 
 let listaCategorias = "";
-let containerChecks = document.querySelector("#container-inputs")
-for ( category of categorias) {
-  listaCategorias += checkBoxes(category)
+let containerChecks = document.querySelector("#container-inputs");
+
+for (category of categorias) {
+  listaCategorias += checkBoxes(category);
 }
+
 containerChecks.innerHTML = listaCategorias;
 
-// 
-
-
-let catCheck = document.querySelectorAll(".form-check-input")
+let catCheck = document.querySelectorAll(".form-check-input");
 for (check of catCheck) {
   check.addEventListener("change", () => {
-    let chequeado = []
+    let chequeado = [];
+
     for (let tic of catCheck) {
       if (tic.checked) { 
         chequeado.push(tic.value)
+      }
     }
-  }
-  console.log(chequeado);
-       if (chequeado.length > 0) {
-          let tarjets = "";
-          let containerCard = document.getElementById("container-cards")
-     data.events.filter(evento => chequeado.includes(evento.category)).forEach(evento => {
-    tarjets += createCard(evento)
-    })
- containerCard.innerHTML = tarjets;       
-  }
-});
-};
 
-let resultados = []; //array vacio para introducir las busquedas.
+    console.log(chequeado);
 
+    if (chequeado.length > 0) {
+      let tarjets = "";
+      let containerCard = document.getElementById("container-cards");
+
+      data.events.filter(evento => chequeado.includes(evento.category)).forEach(evento => {
+        tarjets += createCard(evento)
+      });
+
+      containerCard.innerHTML = tarjets;
+    }
+  });
+}
+
+let resultados = []; // array vacío para introducir las búsquedas.
 let inputBusqueda = document.getElementById("search");
 
-document.querySelector("#form-busqueda").onsubmit = (e) =>{
-  e.preventDefault(); // no se actualiza la pagina con cada envio de form
+document.querySelector("#form-busqueda").onsubmit = (e) => {
+  e.preventDefault(); // no se actualiza la página con cada envío de formulario.
 
-let textIngresado = inputBusqueda.value.toLowerCase().trim(); 
-  resultados = [];
-    for (let event of data.events){
-    if( event.name.toLowerCase().includes(textIngresado) ||                 
-    event.description.toLowerCase().includes(textIngresado)
-     ) {
-      resultados.push(event);
-      }
-  }
-    for (let resultado of resultados) {
-    console.log(resultado);
-  }
-}
+  let formBusqueda = document.querySelector("#form-busqueda");
+  let wordIngresada = document.getElementById("search");
 
-let formBusqueda = document.querySelector("#form-busqueda");
-let wordIngresada = document.getElementById("search").value;
+  formBusqueda.addEventListener("submit", (evento) => {
+    evento.preventDefault(); 
 
-    formBusqueda.addEventListener("submit", (evento) => {
-    evento.preventDefault(); //EVITA QUE SE ENVIE EL FORM AUTOMATICAMENTE.
+    let search = wordIngresada.value.toLowerCase().trim();
+    let result = buscar(search, data.events);
+    mostrarResultados(result);
 
-let search = wordIngresada.trim();
-let result = buscar(search);
-mostrarResultados(result);
-})
-console.log(wordIngresada);
+    console.log(result);
+    console.log(search);
+  });
+};
 
-function buscar(){
-  let result = [];
+function buscar(textBuscar, arrayBusqueda) {
+  let result = arrayBusqueda.filter(elementoB => elementoB.name.toLowerCase().includes(textBuscar) || elementoB.description.toLowerCase().includes(textBuscar));
+
   return result;
 }
-function mostrarResultados(result){
-  let verResultado = document.querySelector("#resultados-busqueda");
-  if(result.length > 0){
-  let htmlResultados = "<ul>" + result.map(r => "<li>" + r + "</li>").join("") + "</ul>";
-  verResultado.innerHTML = htmlResultados;
 
+function mostrarResultados(result) {
+  if (result.length > 0) {
+    let htmlResultados = "";
+
+    for (let r of result) {
+      htmlResultados += createCard(r);
+    }
+
+    tarjets.innerHTML = htmlResultados;
   } else {
-    verResultado.innerHTML = "<p> No results found for the search. Try again.</p>";
+    tarjets.innerHTML = "<p>No results found for the search. Try again.</p>";
+  }
 }
-}
-
-
